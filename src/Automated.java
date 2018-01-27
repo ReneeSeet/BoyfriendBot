cd1423519696915a47f88f43be3cdd910bd2c9fd
Marlene Koh <marlenekohxml@gmail.com>
2018-01-27 19:41:06 +0800

    added more scenarios for user input

--- src/Automated.java
+++ src/Automated.java
@@ -1,4 +1,5 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
@@ -10,10 +11,27 @@ public class Automated extends Bot{
 
     public void checkTime(ArrayList<User> userArr, int index) {
 
+        long chat_id = userArr.get(index).getChatId();
+
         if (currentTime.getHours() == 19 && !userArr.get(index).getAsked()) {
 
-            SendMessage sendMessage = new SendMessage().setChatId(userArr.get(index).getChatId()).setText("Have you eaten?\n");
+            SendMessage sendMessage = new SendMessage().setChatId(chat_id).setText("Have you eaten?\n");
             userArr.get(index).setAsked(true);
+            try {
+                sendMessage(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chat_id);
+            sendPhoto.setPhoto("https://pm1.narvii.com/5813/46199134d8949225c26bbc0d5355510aa46f68d7_hq.jpg");
+            try {
+                sendPhoto(sendPhoto);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            sendMessage = new SendMessage().setChatId(chat_id).setText("I'm eating now haha\n");
             try {
                 sendMessage(sendMessage);
             } catch (TelegramApiException e) {
--- src/UserInput.java
+++ src/UserInput.java
@@ -22,6 +22,7 @@ public class UserInput extends Bot{
     public SendMessage handleUserInput(Update update) {
         SendMessage sendmessage = null;
         long chat_id = update.getMessage().getChatId();
+        
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             String msgtext = update.getMessage().getText().toLowerCase();
@@ -29,26 +30,45 @@ public class UserInput extends Bot{
             if (msgtext.equals("/start")) {
                 sendmessage = new SendMessage().setChatId(chat_id).setText("Hello\n");
             }
-            else if (msgtext.contains("doing")) {
-                sendmessage = new SendMessage().setChatId(chat_id).setText("Thinking of you <3\n");
-            }
-            else if (msgtext.contains("fuck") || msgtext.contains("stress")) {
-                sendmessage = new SendMessage().setChatId(chat_id).setText("Chill girl (:\n");
-            }
-            else if (msgtext.contains(";") ){
-                String[] strArr = msgtext.split(";");
+            else if (msgtext.contains("/") ){
+                String[] strArr = msgtext.split("/");
                 Random r = new Random();
-                int choice = r.nextInt(strArr.length) + 1;
+                int choice = r.nextInt(strArr.length);
                 sendmessage = new SendMessage().setChatId(chat_id).setText(strArr[choice]+"\n");
             }
-            else if (msgtext.contains("sad")||msgtext.contains("stress")) {
+            if (msgtext.contains("doing")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("Thinking of you ❤️\n");
+            }
+            if (msgtext.contains("fuck") || msgtext.contains("stress")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("Chill girl (:\n");
+            }
+
+            if (msgtext.contains("break up") || msgtext.contains("give up") || msgtext.contains("quit") || msgtext.contains("bye")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("noooooooooo :(\n");
+            }
+            if (msgtext.equals("hi")|| msgtext.equals("hi ") || msgtext.contains("hello")||msgtext.equals("yo") || msgtext.contains("hey")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("what's up babe?\n");
+            }
+            if (msgtext.contains("sad")||msgtext.contains("stress")) {
                 sendmessage = new SendMessage().setChatId(chat_id).setText("sendSadSticker");
             }
-            else if (msgtext.contains("love you")) {
+            if (msgtext.contains("love you")) {
                 sendmessage = new SendMessage().setChatId(chat_id).setText("sendLoveSticker");
             }
-            else if (msgtext.contains("funny") || msgtext.contains("joke")) {
+            if (msgtext.contains("hate you")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("but I love you ❤️\n");
+            }
+            if (msgtext.contains("funny") || msgtext.contains("joke")) {
                 sendmessage = new SendMessage().setChatId(chat_id).setText("sendFunnySticker");
+            }
+            if (msgtext.contains("who are you") || msgtext.contains("your name")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("I'm Zames baby ;)\n");
+            }
+            if (msgtext.contains("how old are you") || msgtext.contains("your age")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("21\n");
+            }
+            if (msgtext.contains("how are you")) {
+                sendmessage = new SendMessage().setChatId(chat_id).setText("great! what about you?\n");
             }
             else {
                 sendmessage = new SendMessage().setChatId(chat_id).setText("what lol\n");
